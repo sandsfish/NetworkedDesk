63f8954c823a52bda5196ce81eb58e3b76be6ba8
Sands Fish <sandsfish@gmail.com>
2015-12-12 22:40:07 -0500

    Adding updated Rhino models and exports for shopbotting; Update to NeoPixel code for long strips of 60x.

--- ping_to_neopixel/ping_to_neopixel.ino
+++ ping_to_neopixel/ping_to_neopixel.ino
@@ -6,13 +6,17 @@
 
 #define NEO_PIN 5
 #define PROXIMITY_THRESHOLD 150
-#define MIN_BRIGHTNESS 0
-#define MAX_BRIGHTNESS 175
+#define MIN_BRIGHTNESS 10
+#define MAX_BRIGHTNESS 75
 #define filterSamples 27                // filterSamples should  be an odd number, no smaller than 3
 int sensSmoothArray1 [filterSamples];   // array for holding raw sensor values for ping
 int smoothed;                           // variables for ping data
 const int pingPin = 7;
-
+#define STRIP_PIXELS_PER_METER 60
+#define STRIP_METER_COUNT 1
+#define LED_COUNT STRIP_PIXELS_PER_METER*STRIP_METER_COUNT
+#define LONG_STRIP_BITSTREAM NEO_KHZ800
+#define SHORT_STRIP_BITSTREAM NEO_KHZ400
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -21,27 +25,92 @@ const int pingPin = 7;
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, NEO_PIN, NEO_GRB + NEO_KHZ400);
-int brightness = 127;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, NEO_PIN, NEO_GRB + SHORT_STRIP_BITSTREAM);
+int brightness = MIN_BRIGHTNESS;
 bool neo = HIGH;
+bool idle = LOW;
+int incomingByte = 0;   // for incoming serial data
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
   strip.begin();
+
+  colorWipe(strip.Color(127, 127, 127), 20);
+  
   for(uint16_t i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(255, 255, 255));
+    strip.setPixelColor(i, strip.Color(0, 127, 0)); //  GREEN
   }
-  strip.setBrightness(brightness);
+
+//  setColorAll(strip.Color(0, 127, 0));
+  strip.setBrightness(20);
   strip.show();
+
+//  delay(2000);
+
+//  setColorAll(strip.Color(MIN_BRIGHTNESS, MIN_BRIGHTNESS, MIN_BRIGHTNESS));
+//  strip.setBrightness(20);
+//  strip.show();
+}
+
+void setColorAll(uint32_t color) {
+  for(uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
 void loop() {
 
   // TODO: Check for messages from serial port (or use an interrupt)
   //       If there is a message, run the response routine,
-  //       i.e. flash the NeoPixels red if foodCam or HB100 trigger
+//       i.e. flash the NeoPixels red if foodCam or HB100 trigger
 
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    rainbow(20);
+  }
   
   // Adjust brightness every other loop to make the NeoPixel happy
   neo = !neo;
@@ -67,7 +136,15 @@ void loop() {
   smoothed = digitalSmooth(duration, sensSmoothArray1);
   cm = microsecondsToCentimeters(smoothed);
 
-  if(neo && cm < PROXIMITY_THRESHOLD) {
+  if(neo && cm <= PROXIMITY_THRESHOLD) {
+    if(idle) {
+      idle = LOW;
+      setColorAll(strip.Color(0, 0, 127));  // BLUE
+    }
+    // Clamp the value to our upper limit if we get Ping values larger
+    if(cm > PROXIMITY_THRESHOLD) {
+      cm = PROXIMITY_THRESHOLD;
+    }
     Serial.print(inches);
     Serial.print("in, ");
     Serial.print(cm);
@@ -76,13 +153,19 @@ void loop() {
     brightness = map(cm, PROXIMITY_THRESHOLD, 10, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
     strip.setBrightness(brightness);
     strip.show();
-  } else {
+  } 
+
+  // If we're running this cycle and there is nobody closer than the distance threshold
+  if(neo && cm > PROXIMITY_THRESHOLD) {
     // TODO: This should ramp down the current brightness value until activity
     //       triggers a ramp back up again, from the block above.
-    strip.setBrightness(1);
+    setColorAll(strip.Color(127, 0, 0));  // RED
+    strip.setBrightness(MIN_BRIGHTNESS);
+    strip.show();
+    idle = HIGH;
   }
   
-  delay(15);
+  delay(20);
 }
 
 long microsecondsToCentimeters(long microseconds) {
